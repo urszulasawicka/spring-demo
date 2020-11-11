cbb27e5446634e90b91d3e51a6fca2cab7f8b94e
urszulasawicka <urszulasawicka0@gmail.com>
2020-11-11 19:17:19 +0100

    Default Component Names

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,33 +9,44 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		
-		ClassPathXmlApplicationContext singletonScopeApplicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+		// read spring config file
+		ClassPathXmlApplicationContext singletonScopeApplicationContext = new ClassPathXmlApplicationContext(
+				"applicationContext.xml");
+
+		// get the bean from spring container
 		Coach coach = singletonScopeApplicationContext.getBean("myCoach", Coach.class);
-		
+
+		// call a method on the bean
 		System.out.println(coach.getDailyWorkout());
 		System.out.println(coach.getDailyFortune());
-		
+
 		CricketCoach cricketCoach = singletonScopeApplicationContext.getBean("myCricketCoach", CricketCoach.class);
-		
+
 		System.out.println(cricketCoach.getDailyWorkout());
 		System.out.println(cricketCoach.getDailyFortune());
 		System.out.println(cricketCoach.getEmailAddress());
 		System.out.println(cricketCoach.getTeam());
 		
-		singletonScopeApplicationContext.close();
+		Coach theTennisCoach  = singletonScopeApplicationContext.getBean("tennisCoach", Coach.class);
+		System.out.println(theTennisCoach.getDailyWorkout());
 		
-		ClassPathXmlApplicationContext beanScopeApplicationContext = new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");		
-	
+		// close the context
+		singletonScopeApplicationContext.close();
+
+		ClassPathXmlApplicationContext beanScopeApplicationContext = new ClassPathXmlApplicationContext(
+				"beanScope-applicationContext.xml");
+
 		Coach beanScopeCoach = beanScopeApplicationContext.getBean("myCoach", Coach.class);
 		Coach alphaBeanScopeCoach = beanScopeApplicationContext.getBean("myCoach", Coach.class);
-		
+
 		System.out.println("Pointing to the same object: " + (beanScopeCoach == alphaBeanScopeCoach));
-		
+
 		beanScopeApplicationContext.close();
-		
-		ClassPathXmlApplicationContext beanLifecycleApplicationContext = new ClassPathXmlApplicationContext("beanLifecycle-applicationContext.xml");		
-		
+
+		ClassPathXmlApplicationContext beanLifecycleApplicationContext = new ClassPathXmlApplicationContext(
+				"beanLifecycle-applicationContext.xml");
+
 		Coach beanLifecycleCoach = beanLifecycleApplicationContext.getBean("myCoach", Coach.class);
 
 		beanLifecycleApplicationContext.close();
